7695ab705ae68e2a6999cf561dd28cbfaf726466
Adam Terrey <arterrey@gmail.com>
2021-07-24 14:47:12 +0700

    Use site object for gaining identity and permits assertions

--- olcommon/http/security.py
+++ olcommon/http/security.py
@@ -13,9 +13,9 @@ class SecurityPolicy:
     def identity(self, request):
         claims = request.jwt_claims
         if claims and "access" in claims.get("aud", []):
-            return request.root.identity_for_jwt_claims(claims)
+            return request.site.identity_for_jwt_claims(claims)
         return None
 
     def permits(self,request, context, permission):
-        principals = request.root.principals_for_identity(request.identity)
+        principals = request.site.principals_for_identity(request.identity)
         return ACLHelper().permits(context, principals, permission)
