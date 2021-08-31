ee3552f75001a1b0004fc5a47cdad855ebcaa899
Adam Terrey <arterrey@gmail.com>
2021-08-31 08:47:09 +0700

    Add Google and SMTP logging. Refactor actor extra items

--- olcommon/logging.py
+++ olcommon/logging.py
@@ -12,20 +12,14 @@ class ActorLoggerAdapter(logging.LoggerAdapter):
         }
 
         # If we have a request in the extra then override actor and actor_ip
-        if request := eelf.extra.get("request"):
+        if request := self.extra.get("request"):
             extra["actor"] = request.unauthenticated_userid  # One day this can be str(request.identity)
             extra["actor_ip"] = request.client_addr
-            del extra["request"]  # Remove as this is not serializable in many cases
-
-        # Init actor_formated with either actor or Anonymous
-        if extra["actor"]:
-            extra["actor_formated"] = extra["actor"]
         else:
-            extra["actor_formated"] = "Anonymous"
+            extra["actor"] = extra.get("actor")
+            extra["actor_ip"] = extra.get("actor_ip")
 
-        # If we have an IP then add it to the formatted value
-        if extra["actor_ip"]:
-            extra["actor_formated"] += f" ({self.extra['actor_ip']})"
+        extra["actor_formatted"] = f'{extra["actor_ip"] or "-"} {extra["actor"] or "-"}'
 
         # Construct kwargs and return
         kwargs = {
@@ -37,11 +31,49 @@ class ActorLoggerAdapter(logging.LoggerAdapter):
 
 class GoogleLoggingJSONFormatter(VerboseJSONFormatter):
 
-    PATTERN_REQUEST_URL = re.compile(r"https?://[][A-Za-z0-9._~:/?#[@!$&'()*+,;%=-]*")
-
     def json_record(self, message, extra, record):
-        request_url_mo = self.PATTERN_REQUEST_URL.search(message)
-        if request_url_mo:
-            extra["logging.googleapis.com/httpRequest"] = {"requestUrl": request_url_mo.group(0)}
         extra['logging.googleapis.com/severity'] = record.levelname
+        if actor := getattr(record, "actor"):
+            extra["actor"] = actor
+        if actor_ip := getattr(record, "actor_ip"):
+            extra["actorIp"] = actor_ip
+        if request_method := getattr(record, "request_method"):
+            extra["requestMethod"] = request_method
+        if request_url := getattr(record, "request_url"):
+            extra["requestUrl"] = request_url
+        if response_status := getattr(record, "response_status"):
+            extra["responseStatus"] = response_status
+        if response_content_length := getattr(record, "response_content_length"):
+            extra["responseContentLength"] = response_content_length
+        if user_agent := getattr(record, "user_agent"):
+            extra["userAgent"] = user_agent
+        if referer := getattr(record, "referer"):
+            extra["referer"] = referer
+        if route_name := getattr(record, "route_name"):
+            extra["routeName"] = route_name
+        if view_name := getattr(record, "view_name"):
+            extra["viewName"] = view_name
+        if latency := getattr(record, "latency"):
+            extra["latency"] = latency
         return super().json_record(message, extra, record)
+
+class FormatterSetDefaults(logging.Formatter):
+
+    def format(self, record):
+        if not hasattr(record, "actor_ip"):
+            record.actor_ip = None
+        if not hasattr(record, "actor"):
+            record.actor = None
+        if not hasattr(record, "actor_formatted"):
+            record.actor_formatted = "- -"
+        message = super().format(record)
+        return message
+
+
+class CustomSubjectSMTPHandler(logging.handlers.SMTPHandler):
+
+    def getSubject(self, record):
+        subject = self.subject.format(**record.__dict__)
+        if record.exc_info is not None:
+            subject += f": {record.exc_info[0].__name__}"
+        return subject
