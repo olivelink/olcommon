449038708cf5bbe228d9bb275d28ad5e6ce53663
Adam Terrey <adam@olivelink.net>
2021-11-04 08:38:55 +0000

    Add enqueue_emit jobe behavior

--- olcommon/jobs.py
+++ olcommon/jobs.py
@@ -1,25 +1,21 @@
 from ctq import emit
 
 
-def site_call(site, method_name, *args, **kwargs):
-    func = getattr(site, method_name)
-    func(*args, **kwargs)
-
-
 def resource_call(site, path, method_name, *args, **kwargs):
-    context = {"": site}
-    if isinstance(path, str):
-        path = path.split("/")
-    for key in path:
-        context = context[key]
+    context = _traverse(site, path)
     func = getattr(context, method_name)
     func(*args, **kwargs)
 
 
 def resource_emit(site, path, event_name, *args, **kwargs):
-    context = {"": site}
+    context = _traverse(site, path)
+    emit(context, event_name, *args, **kwargs)
+
+
+def _traverse(root, path):
+    context = {"": root}
     if isinstance(path, str):
         path = path.split("/")
     for key in path:
-        context = context[key]
-    emit(context, event_name, *args, **kwargs)
+        context = context[key]   
+    return context
